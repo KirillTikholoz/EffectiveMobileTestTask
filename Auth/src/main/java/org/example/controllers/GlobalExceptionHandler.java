a4aed87ef039c4a672f0a5e148d982273c255d70
thlz <118001422+KirillTikholoz@users.noreply.github.co>
2026-02-05 15:13:30 +0400

    auth: changed the exception handling

--- Auth/src/main/java/org/example/controllers/GlobalExceptionHandler.java
+++ Auth/src/main/java/org/example/controllers/GlobalExceptionHandler.java
@@ -1,16 +1,66 @@
 package org.example.controllers;
 
+import io.jsonwebtoken.JwtException;
 import lombok.extern.slf4j.Slf4j;
 import org.example.dtos.ErrorResponseDto;
+import org.example.exceptions.PasswordMismatchException;
+import org.example.exceptions.UserAlreadyExistsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingRequestCookieException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
+    @ExceptionHandler(PasswordMismatchException.class)
+    public ResponseEntity<ErrorResponseDto> PasswordMismatchExceptionHandler(PasswordMismatchException ex) {
+        log.error(ex.getMessage());
+        return new ResponseEntity<>(
+                new ErrorResponseDto("Bad Request", "Password and confirmation password do not match"),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponseDto> UserAlreadyExistsExceptionHandler(UserAlreadyExistsException ex) {
+        log.error(ex.getMessage());
+        return new ResponseEntity<>(
+                new ErrorResponseDto("Bad Request", "Such a user already exists"),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler(MissingRequestCookieException.class)
+    public ResponseEntity<ErrorResponseDto> illegalArgumentExceptionHandler(MissingRequestCookieException ex) {
+        log.error(ex.getMessage());
+        return new ResponseEntity<>(
+                new ErrorResponseDto("Bad Request", "Required cookie 'refreshToken' is not present"),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ErrorResponseDto> badCredentialsExceptionHandler(BadCredentialsException ex) {
+        log.error(ex.getMessage());
+        return new ResponseEntity<>(
+                new ErrorResponseDto("Unauthorized", "Incorrect email or password"),
+                HttpStatus.UNAUTHORIZED
+        );
+    }
+
+    @ExceptionHandler(JwtException.class)
+    public ResponseEntity<ErrorResponseDto> jwtExceptionHandler(JwtException ex) {
+        log.error(ex.getMessage());
+        return new ResponseEntity<>(
+                new ErrorResponseDto("Unauthorized", "Invalid jwt token"),
+                HttpStatus.UNAUTHORIZED
+        );
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponseDto> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException ex) {
         log.error(ex.getMessage());
